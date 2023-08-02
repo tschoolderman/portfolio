bef5654647672111db41684ee4b3d9e7d0057714
tschoolderman <t.d.schoolderman@gmail.com>
2023-08-02 19:18:25 +0200

    Finish up the simple webscraper

--- app/webscraper/scraper.py
+++ app/webscraper/scraper.py
@@ -14,20 +14,29 @@ class Scraper:
     def make_soup(self, html):
         self.soup = BeautifulSoup(html.text, "lxml")
 
-    def get_title(self):
+    def get_tab_title(self):
         self.title = self.soup.select("title")[0].getText()
 
-    def get_authors(self):
-        for author in self.soup.select(".author"):
-            self.result.append(author.text)
+    def get_elements(self):
+        if self.soup.select(".author"):
+            for element in self.soup.select(".author"):
+                self.result.append(element.text)
 
-    def run(self):
-        response = self.get_url("https://quotes.toscrape.com/")
+        elif self.soup.select(".title"):
+            for element in self.soup.select(".title"):
+                self.result.append(element.text)
+
+        elif self.soup.select(".h6"):
+            for element in self.soup.select(".h6"):
+                self.result.append(element.text)
+
+    def run(self, url):
+        response = self.get_url(url)
         self.make_soup(response)
-        self.get_title()
-        self.get_authors()
+        self.get_tab_title()
+        self.get_elements()
 
 
 # if __name__ == "__main__":
 #     scraper = Scraper()
-#     scraper.run()
+#     scraper.run("https://dekudeals.com/")
